244b7650490bdf8b3de94af090b7681279332ed9
Yury Fil <yury.fil@nordigy.ru>
2021-12-25 23:49:33 +0100

    Irdeto task 2 initial commit

---
+++ irdeto/2/math_function.py
@@ -0,0 +1,20 @@
+# TODO: handle 1/128 delta
+
+def math_function(x: int):
+    # handle delta:
+    if x % (1 / 128) != 0.0:
+        raise ValueError(f'Value increment step should equal to 1/128. '
+                         f'Input value: {x}. x mod 1/128 = {x % (1 / 128)}')
+    if -10 <= x <= -2:
+        return -(x*x)
+    elif (-2 < x < 8) and (x != -1):
+        return (1 - x) / (1 + x)
+    elif 8 < x <= 35:
+        return abs(x - 12)
+    else:
+        raise ValueError(f'Function accepts values in ranges [-10, -2], (-2, 8), x!=1 and (8, 35]. '
+                         f'Input value: {x}')
+
+
+if __name__ == "__main__":
+    math_function(1 + 1/256)
